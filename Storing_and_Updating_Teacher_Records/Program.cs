970b9f4198413d805c71f97e2dfd8e00bcff2368
Bikram7852 <bikram.roy7852@gmail.com>
2023-10-30 13:05:20 +0530

    Add project files.

---
+++ Storing_and_Updating_Teacher_Records/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeacherLibrary;
+
+namespace Storing_and_Updating_Teacher_Records
+{
+    internal class Program
+    {
+        private static string teacherDataFilePath = "teacher_data.txt";
+        static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher(1, "John Doe", "Class 10A");
+            StoreTeacherData(teacher);
+            Console.WriteLine("Before updating the teacher data: \n" + "Id = " + teacher.Id + " " + "\nName = " + teacher.Name + " " + "\nClass and Section = " + teacher.ClassAndSection);
+            teacher.Name = "John Smith";
+            teacher.ClassAndSection = "Class 12A";
+            UpdateTeacherData(teacher);
+            Teacher teacherData = GetTeacherData(1);
+            Console.WriteLine();
+            Console.WriteLine("After updating the teacher data: \n" + "Id = " + teacherData.Id + " " + "\nName = " + teacherData.Name + " " + "\nClass and Section = " + teacherData.ClassAndSection);
+            Console.ReadLine();
+        }
+        private static void StoreTeacherData(Teacher teacher)
+        {
+            using (StreamWriter sw = new StreamWriter(teacherDataFilePath, true))
+            {
+                sw.WriteLine($"{teacher.Id},{teacher.Name},{teacher.ClassAndSection}");
+            }
+        }
+
+        private static void UpdateTeacherData(Teacher teacher)
+        {
+            List<string> teacherDataLines = new List<string>();
+
+            using (StreamReader sr = new StreamReader(teacherDataFilePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    teacherDataLines.Add(line);
+                }
+            }
+
+            for (int i = 0; i < teacherDataLines.Count; i++)
+            {
+                string[] teacherDataFields = teacherDataLines[i].Split(',');
+
+                if (teacherDataFields[0] == teacher.Id.ToString())
+                {
+                    teacherDataLines[i] = $"{teacher.Id},{teacher.Name},{teacher.ClassAndSection}";
+                    break;
+                }
+            }
+
+            using (StreamWriter sw = new StreamWriter(teacherDataFilePath, false))
+            {
+                foreach (string teacherDataLine in teacherDataLines)
+                {
+                    sw.WriteLine(teacherDataLine);
+                }
+            }
+        }
+
+        private static Teacher GetTeacherData(int teacherId)
+        {
+            Teacher teacher = null;
+
+            using (StreamReader sr = new StreamReader(teacherDataFilePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] teacherDataFields = line.Split(',');
+
+                    if (teacherDataFields[0] == teacherId.ToString())
+                    {
+                        teacher = new Teacher(teacherId, teacherDataFields[1], teacherDataFields[2]);
+                        break;
+                    }
+                }
+            }
+
+            return teacher;
+        }
+    }
+}
---
+++ TeacherLibrary/Teacher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherLibrary
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ClassAndSection { get; set; }
+
+        public Teacher(int id, string name, string classAndSection)
+        {
+            Id = id;
+            Name = name;
+            ClassAndSection = classAndSection;
+        }
+    }
+}
